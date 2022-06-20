f2edc9360849a5dc68d690cd930565c6deccc080
victorsanchezgimenez <victorsanchezgimenez98@gmail.com>
2022-06-20 18:10:28 +0200

    Master tower defense start

---
+++ Curriculum game/Assets/Scripts/TowerDefense/EnemyMover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
+
+    
+    void Start() 
+    {
+        StartCoroutine(FollowPath());    
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path) {
+        {
+            transform.position = new Vector3(waypoint.transform.position.x, 0, transform.position.z);
+            yield return new WaitForSeconds(waitTime);
+        }
+        }
+    }
+}
